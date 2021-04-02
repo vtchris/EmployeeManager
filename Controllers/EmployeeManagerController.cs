e2b469f32825d3f8e8ab848035aad00caab9ce87
VTC <vtchris.2357@gmail.com>
2021-04-02 09:39:44 -0400

    initial files

---
+++ Controllers/EmployeeManagerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManager.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+        private AppDbContext db = null;
+        public EmployeeManagerController(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult List()
+        {
+            List<Employee> model = (from e in db.Employees orderby e.EmployeeID select e).ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            FillCountries();
+            return View();
+        }
+
+        private void FillCountries()
+        {
+           // List<SelectList>
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EmployeeManager
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("NorthwindDb")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+           
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
+            });
+        }
+    }
+}
